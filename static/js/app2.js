ec29f6b1b1f7e8412830f6b68dfe4003ed8d4707
katrahan01 <66267629+katrahan01@users.noreply.github.com>
2020-09-26 13:54:05 -0500

    updating

--- static/js/app2.js
+++ static/js/app2.js
@@ -27,14 +27,18 @@ button.on("click", function() {
 
     tbody.html("");
 
-    // Select the input date get the raw HTML nodes
-    var inputElement = d3.select("#datetime");
+    // Select the input date, state, shape and get the raw HTML nodes
+    var inputElement = d3.select("#input");
     // Get the value property of the input date, state, shape
     var inputValue = inputElement.property("value");
     // console.log input value
-    console.log(inputValue);
+    // console.log(inputValue);
     // Filter Data with datetime equal to input value
-    var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
+    var filteredData = tableData.filter(sighting => sighting.datetime === inputValue ||
+                                                    sighting.city === inputValue ||
+                                                    sighting.state === inputValue ||
+                                                    sighting.country === inputValue ||
+                                                    sighting.shape === inputValue);
     // console.log filter values
     console.log(filteredData);
 
